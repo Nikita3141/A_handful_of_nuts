1bf7e2081f09308f7be9534c8e67d3c7ecb23f36
Nikita3141 <71587595+Nikita3141@users.noreply.github.com>
2021-02-11 21:35:52 +0300

    Add final version

--- server.cpp
+++ server.cpp
@@ -1,52 +1,87 @@
-#include <ststream.h>
-#include "message.h"
+#include<iostream>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include <netinet/in.h>
+#include<unistd.h>
 #include <string.h>
-#include <signal.h>
-#include <ctime>
+//Just
 
-int main()
 
-{
-	int 	len, pid, cmdId;
-	char	buf[256];
-	time_t	tim;
 
-	//Настроить этот процесс как демон
-	for(int i = 0; i < 20;i++) signal(i,SIG_IGN);
-	setsid();
-	cout<<"server: start executing...\n" << flush;
-	message mqueue (MSGKEY);
-	if(!mqueue.good()) exit(1);
-
-	//Ожидать запрос от клиента
-	while(len = mqueue.rcv(buf,sizeof(buf),-99,&cmdId) > 0){
-		//Получить идентификатор процесса клиента и проверить его достоверность
-		istrstream(buf,sizeof(buf))>>pid;
-		if(pid < 100){
-			cerr << "Illegal PID: " << buf << " " << pid << endl;
-			continue;
-		}
-		//Подготовить ответ клиенту
-		cerr << "server: recieve cmd #" << cmdId << "from client:"<<pid<<endl;
-		switch(cmdId){
-
-			case LOCAL_TIME:
-				tim = time(0);
-				strcpy(buf,ctime(&tim));
-				break;
-			case UTC_TIME:
-				tim = time(0);
-				strcpy(buf,asctime(gmtime(&tim)));
-				break;
-			case QUIT_CMD:
-				cerr << "server: deleting msg queue ...\n";
-				return mqueue.rmQ();
-			default:
-				ostrstream(buf,sizeof(buf))<<"Illegal cmd:" << cmdId << '\0;
-		}
-		//Послать клиенту ответ
-		if(mqueue.send(buf,strlen(buf)+1,pid)==-1)
-		cerr << "Server: "<<getpid() << "send response fails\n";
-	} // Бесконечный цикл
-	return 0;
+using namespace std;
+int Socket(int domain, int type, int protocol) {
+    int res = socket(domain, type, protocol);
+    if (res==-1) {
+        perror("error");
+        exit(EXIT_FAILURE);
+    }
+    return res;
 }
+// подключаем
+void Bind(int sockfd,const struct sockaddr *addr,socklen_t addrlen){
+    int res=bind(sockfd, addr, addrlen);
+    if (res==-1) {
+        perror("error");
+        exit(EXIT_FAILURE);
+    }
+}
+void Listen(int sockfd,int backlog) {
+    int res=listen(sockfd,backlog);
+    if (res == -1) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+}
+int Accept(int sockfd,struct sockaddr *addr, socklen_t *addrlen){
+    int res=accept(sockfd,addr,addrlen);
+    if(res==-1){
+        perror("error");
+        exit(EXIT_FAILURE);
+    }
+    return res;
+}
+
+int main(){
+    int server=Socket(AF_INET, SOCK_STREAM,0); //создаем сокет и записываем дескриптор
+    struct sockaddr_in adr={0}; //  задать адрес по протоколу IPV4
+    adr.sin_family=AF_INET; // семейство адресов
+    adr.sin_port=htons(80); // от хоста к сети (порт на котором сервер будет слушать)
+                                    // (htons- изменение порядка байт)
+    adr.sin_addr.s_addr =INADDR_ANY;
+
+    Bind(server, (struct sockaddr *) &adr, sizeof adr); // привязка сокета к адресу (server к adr)
+    Listen(server,5); // слушаем на сокете server до 5 клиентов в очереди
+    socklen_t adrlen=sizeof adr;  // запоминаем размер адреса
+    int fd=Accept(server,(struct sockaddr *) &adr,&adrlen); // принимаем клиента и собираем в adr инфу о клиенте
+    ssize_t nread; // кол-во считываемых байт
+    char buf[256];
+    while(true){
+        memset(buf,0,256); // чистим буфер
+        int Byte_recv= recv(fd, buf, 256,0); // принимаем строку от клиента
+        if(Byte_recv==-1){
+            cout<<"error 1"<<endl;
+            break;
+        }
+        else if(Byte_recv==0){
+            cout<<"error 2"<<endl;
+            break;
+        }
+        cout<<"Recieved: "<<string(buf,0,Byte_recv)<<endl; // вывод от клиента
+        send(fd,buf,Byte_recv+1,0); // отсылаем клиенту ответ на его сообщение
+    }
+    sleep(1);
+    close(fd);
+    close(listen(server,5));
+}
+//    nread=read(fd,buf,256); // максимально 256 символов (в nread реально считанное кол-во байт)
+//    if (nread==-1){
+//        perror("read failed");
+//        exit(EXIT_FAILURE);
+//    }
+//    if (nread==0){
+//        printf("END OF FILE\n");
+//    }
+//    write(STDOUT_FILENO,buf,nread);
+//    write(fd,buf,nread); // на fd передаем buf и nread
+//    recv(fd, buf, nread,0);
