ccaebc650d5f71632b389f62d6cc852f52e88133
Nikita3141 <71587595+Nikita3141@users.noreply.github.com>
2021-02-15 22:25:43 +0300

    Add new files

--- client.cpp
+++ client.cpp
@@ -1,12 +1,9 @@
-#include <sys/types.h>
 #include <sys/socket.h>
 #include<iostream>
 #include<stdio.h>
 #include <netinet/in.h>
-#include<unistd.h>
 #include <arpa/inet.h>
 #include <string.h>
-//Just a comment
 using namespace std;
 void Connect(int sockfd,const struct sockaddr *addr,socklen_t addrlen){
     int res=connect(sockfd,addr,addrlen);
@@ -23,28 +20,15 @@ int Socket(int domain, int type, int protocol) {
     }
     return res;
 }
-//int Inet_pton(int af, const char *src,void *dst){
-//    int res = inet_pton(af,src,dst);
-//    if(res==0){
-//        printf("inet_pton failed, no addres");
-//        exit(EXIT_FAILURE);
-//    }
-//    if(res=-1){
-//        perror("inet_pton failed");
-//        exit(EXIT_FAILURE);
-//    }
-//    return res;
-//}
 int main(){
     int fd=Socket(AF_INET,SOCK_STREAM,0);
     struct sockaddr_in adr={0};
     adr.sin_family=AF_INET;
     adr.sin_port=htons(80);
-    adr.sin_addr.s_addr =INADDR_ANY;
-  //  Inet_pton(AF_INET, ("127.0.0.1"),&adr.sin_addr); // стучимся в адрес (на локалку) и помещаем адрес в adr.sin_Addr
+    //    adr.sin_addr.s_addr =INADDR_ANY; // люой
+    // inet_pton(AF_INET,"192.168.1.101",&adr.sin_addr); // на случай явного задания адреса
     Connect(fd, (struct sockaddr *) &adr, sizeof adr); // выполняем соединение
-//    write(fd, "Hello\n",6); // пишем серверу hello
-    char buf[256];
+    char buf[1024];
     string user_input; // ввод пользователя
     do{
         cout<<">";
@@ -59,6 +43,5 @@ int main(){
         int Byte_recv=recv(fd,buf,256,0); // принимаем ответ от сервера
         cout<<"Server: "<<string(buf,Byte_recv)<<endl; // выводим ответ от сервера
     } while (true);
-
-    close(fd);
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
-#include<sys/types.h>
 #include<sys/socket.h>
 #include<stdio.h>
-#include <netinet/in.h>
+#include<netinet/in.h>
 #include<unistd.h>
-#include <string.h>
-//Just
-
-
+#include<string.h>
+#include <arpa/inet.h>
+#include<thread>
 
 using namespace std;
 int Socket(int domain, int type, int protocol) {
@@ -41,47 +39,41 @@ int Accept(int sockfd,struct sockaddr *addr, socklen_t *addrlen){
     }
     return res;
 }
-
-int main(){
-    int server=Socket(AF_INET, SOCK_STREAM,0); //создаем сокет и записываем дескриптор
-    struct sockaddr_in adr={0}; //  задать адрес по протоколу IPV4
-    adr.sin_family=AF_INET; // семейство адресов
-    adr.sin_port=htons(80); // от хоста к сети (порт на котором сервер будет слушать)
-                                    // (htons- изменение порядка байт)
-    adr.sin_addr.s_addr =INADDR_ANY;
-
-    Bind(server, (struct sockaddr *) &adr, sizeof adr); // привязка сокета к адресу (server к adr)
-    Listen(server,5); // слушаем на сокете server до 5 клиентов в очереди
+void threadFunction(int server,double tmp,struct sockaddr_in adr){
+    Listen(server,1); // слушаем на сокете server
     socklen_t adrlen=sizeof adr;  // запоминаем размер адреса
-    int fd=Accept(server,(struct sockaddr *) &adr,&adrlen); // принимаем клиента и собираем в adr инфу о клиенте
-    ssize_t nread; // кол-во считываемых байт
-    char buf[256];
-    while(true){
-        memset(buf,0,256); // чистим буфер
-        int Byte_recv= recv(fd, buf, 256,0); // принимаем строку от клиента
-        if(Byte_recv==-1){
-            cout<<"error 1"<<endl;
+    int fd=Accept(server,(struct sockaddr *) &tmp,&adrlen); // принимаем клиента и собираем в adr инфу о клиенте
+    char buf[1024];
+    while(true) {
+        memset(buf, 0, 1024); // чистим буфер
+        int Byte_recv = recv(fd, buf, 1024, 0); // принимаем строку от клиента
+        if (Byte_recv == -1) {
+            cout << "error 1" << endl;
+            break;
+        } else if (Byte_recv == 0) {
+            cout << "error 2" << endl;
             break;
         }
-        else if(Byte_recv==0){
-            cout<<"error 2"<<endl;
-            break;
-        }
-        cout<<"Recieved: "<<string(buf,0,Byte_recv)<<endl; // вывод от клиента
-        send(fd,buf,Byte_recv+1,0); // отсылаем клиенту ответ на его сообщение
+        cout << "Recieved: " << string(buf, 0, Byte_recv) << endl; // вывод от клиента
+        send(fd, buf, Byte_recv + 1, 0); // отсылаем клиенту ответ на его сообщение
     }
     sleep(1);
     close(fd);
-    close(listen(server,5));
+    close(listen(server,1));
 }
-//    nread=read(fd,buf,256); // максимально 256 символов (в nread реально считанное кол-во байт)
-//    if (nread==-1){
-//        perror("read failed");
-//        exit(EXIT_FAILURE);
-//    }
-//    if (nread==0){
-//        printf("END OF FILE\n");
-//    }
-//    write(STDOUT_FILENO,buf,nread);
-//    write(fd,buf,nread); // на fd передаем buf и nread
-//    recv(fd, buf, nread,0);
+int main() {
+    int server = Socket(AF_INET, SOCK_STREAM, 0); //создаем сокет и записываем дескриптор
+    struct sockaddr_in adr = {0}; //  задать адрес по протоколу IPV4
+    adr.sin_family = AF_INET; // семейство адресов
+    adr.sin_port = htons(80); // от хоста к сети (порт на котором сервер будет слушать)
+    // (htons- изменение порядка байт)
+    // inet_pton(AF_INET,"192.168.1.104",&adr.sin_addr); // на случай явной привязки адреса
+    adr.sin_addr.s_addr = INADDR_ANY;
+    Bind(server, (struct sockaddr *) &adr, sizeof adr); // привязка сокета к адресу (server к adr)
+    double tmp = sizeof adr;
+    std::thread thread1(threadFunction, server, tmp, adr);
+    std::thread thread2(threadFunction, server, tmp, adr);
+    thread1.join();
+    thread2.join();
+    return 0;
+}
