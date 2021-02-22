23f37b361d80f3859fefa68817d4dc8a76201418
Nikita3141 <71587595+Nikita3141@users.noreply.github.com>
2021-02-22 15:47:09 +0300

    Add files via upload

--- server.cpp
+++ server.cpp
@@ -4,46 +4,53 @@
 #include<netinet/in.h>
 #include<unistd.h>
 #include<string.h>
-#include <arpa/inet.h>
 #include<thread>
-
 using namespace std;
-int Socket(int domain, int type, int protocol) {
-    int res = socket(domain, type, protocol);
-    if (res==-1) {
-        perror("error");
-        exit(EXIT_FAILURE);
+
+class serv{
+public:
+    serv();
+    bool Startserver();
+    void threadFunction();
+private:
+    int server;
+    char buf[1024];
+    struct sockaddr_in adr{};
+    int Socket(int domain, int type, int protocol) {
+        int res = socket(domain, type, protocol);
+        if (res==-1) {
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
+        return res;
     }
-    return res;
-}
-// подключаем
-void Bind(int sockfd,const struct sockaddr *addr,socklen_t addrlen){
-    int res=bind(sockfd, addr, addrlen);
-    if (res==-1) {
-        perror("error");
-        exit(EXIT_FAILURE);
+    void Bind(int sockfd,const struct sockaddr *addr,socklen_t addrlen){
+        int res=bind(sockfd, addr, addrlen);
+        if (res==-1) {
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
     }
-}
-void Listen(int sockfd,int backlog) {
-    int res=listen(sockfd,backlog);
-    if (res == -1) {
-        perror("listen failed");
-        exit(EXIT_FAILURE);
+    void Listen(int sockfd,int backlog) {
+        int res=listen(sockfd,backlog);
+        if (res == -1) {
+            perror("listen failed");
+            exit(EXIT_FAILURE);
+        }
     }
-}
-int Accept(int sockfd,struct sockaddr *addr, socklen_t *addrlen){
-    int res=accept(sockfd,addr,addrlen);
-    if(res==-1){
-        perror("error");
-        exit(EXIT_FAILURE);
+    int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen){
+        int res=accept(sockfd,addr,addrlen);
+        if(res==-1){
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
+        return res;
     }
-    return res;
-}
-void threadFunction(int server,double tmp,struct sockaddr_in adr){
+};
+void serv::threadFunction(){
     Listen(server,1); // слушаем на сокете server
     socklen_t adrlen=sizeof adr;  // запоминаем размер адреса
-    int fd=Accept(server,(struct sockaddr *) &tmp,&adrlen); // принимаем клиента и собираем в adr инфу о клиенте
-    char buf[1024];
+    int fd=Accept(server,(struct sockaddr *) &adr,&adrlen); // принимаем клиента и собираем в adr инфу о клиенте
     while(true) {
         memset(buf, 0, 1024); // чистим буфер
         int Byte_recv = recv(fd, buf, 1024, 0); // принимаем строку от клиента
@@ -61,19 +68,24 @@ void threadFunction(int server,double tmp,struct sockaddr_in adr){
     close(fd);
     close(listen(server,1));
 }
-int main() {
-    int server = Socket(AF_INET, SOCK_STREAM, 0); //создаем сокет и записываем дескриптор
-    struct sockaddr_in adr = {0}; //  задать адрес по протоколу IPV4
+serv::serv() {
+    adr = {0}; //  задать адрес по протоколу IPV4
     adr.sin_family = AF_INET; // семейство адресов
     adr.sin_port = htons(80); // от хоста к сети (порт на котором сервер будет слушать)
     // (htons- изменение порядка байт)
-    // inet_pton(AF_INET,"192.168.1.104",&adr.sin_addr); // на случай явной привязки адреса
     adr.sin_addr.s_addr = INADDR_ANY;
+    server = Socket(AF_INET, SOCK_STREAM, 0); //создаем сокет и записываем дескриптор
     Bind(server, (struct sockaddr *) &adr, sizeof adr); // привязка сокета к адресу (server к adr)
-    double tmp = sizeof adr;
-    std::thread thread1(threadFunction, server, tmp, adr);
-    std::thread thread2(threadFunction, server, tmp, adr);
+}
+bool serv::Startserver() {
+    std::thread thread1(&serv::threadFunction, this);
+    std::thread thread2(&serv::threadFunction, this);
     thread1.join();
     thread2.join();
+    return true;
+}
+int main() {
+    serv server;
+    server.Startserver();
     return 0;
 }
